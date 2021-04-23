a6d94a93a1d362f1945d0f34cd3d35405cec0d20
Max Erastov <max.erastov@gmail.com>
2021-04-24 00:02:21 +0300

    Add tests and factories for current models

---
+++ test/factories/sequence.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :password, \
+                              :avatar, :name, :description, :state] do |n|
+    "string#{n}"
+  end
+
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+end
